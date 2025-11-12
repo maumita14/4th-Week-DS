0d687960790b40d086dd91db7866de64083038ab
maumita14 <senmaumita302@gmail.com>
2025-11-12 20:08:36 +0530

    Implement string concatenation in 3_string_append.c

---
+++ 3_string_append.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char string1[]="Sukanya";
+    char string2[]="Hazarika";
+    strcat(string1,string2);
+    printf("string after append->%s", string1);
+
+}
